00914b6cb44d0ebbd9d4967c8dd6128469db00f0
jcasals <jcasals@gaa9.com>
2017-08-30 09:55:49 +0200

    refactor imports

--- source_code/settings.py
+++ source_code/settings.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import platform
+
 from selenium import webdriver
+
 
 # under different operating systems
 if platform.system() == 'Windows':
@@ -11,7 +13,7 @@ else:
 
 FFPROFILE = webdriver.FirefoxProfile()
 
-# TODO migrate to BaseScraper load
+# TODO (casals) migrate to BaseScraper load
 print('Trying to open with extension')
 FFPROFILE.add_extension(extension='../resources/addblock.xpi')
 print('Finished load extension')
