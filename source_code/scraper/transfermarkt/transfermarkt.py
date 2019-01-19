250f60425dc052a8a5720fe260db1751c569b47a
Nekoshido <hordonvi@gmail.com>
2019-01-19 13:37:15 +0100

    implement feature for create the directory if it does not exist

--- source_code/scraper/transfermarkt/transfermarkt.py
+++ source_code/scraper/transfermarkt/transfermarkt.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from itertools import chain, zip_longest
 import json
+import pathlib
+
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -52,56 +54,62 @@ if __name__ == "__main__":
         browser = webdriver.Firefox(firefox_profile=settings.FFPROFILE, executable_path=settings.EXECUTABLE)
     except Exception:
         browser = webdriver.Firefox(firefox_profile=settings.FFPROFILE)
-    season = constants_transfermarkt.YEARS[constants_transfermarkt.YEARS_INDEX]
-    site = '{}/{}/startseite/wettbewerb/{}/plus/?saison_id={}'.format(constants_transfermarkt.TRANSFERMARKT_URL,
-                                                                      constants_transfermarkt.LEAGUES_LINK[constants_transfermarkt.LEAGUE_INDEX],
-                                                                      constants_transfermarkt.LEAGUES_SHORT[constants_transfermarkt.LEAGUE_INDEX],
-                                                                      constants_transfermarkt.YEARS[constants_transfermarkt.YEARS_INDEX])
-    print(site)
-    browser.get(site)
-    page = browser.page_source
-    soup = BeautifulSoup(page, "html.parser")
+    for year in constants_transfermarkt.YEARS:
 
-    table_teams = soup.find('div', {'id': 'yw1'})
-    teams = table_teams.find_all('a', {'class': 'vereinprofil_tooltip tooltipstered'})
-    list_array = []
-    for team in teams:
-        team_href = team.get('href')
-        if team_href not in list_array:
-            list_array.append(team_href)
+        season = constants_transfermarkt.YEARS[constants_transfermarkt.YEARS_INDEX]
+        site = '{}/{}/startseite/wettbewerb/{}/plus/?saison_id={}'.format(constants_transfermarkt.TRANSFERMARKT_URL,
+                                                                          constants_transfermarkt.LEAGUES_LINK[constants_transfermarkt.LEAGUE_INDEX],
+                                                                          constants_transfermarkt.LEAGUES_SHORT[constants_transfermarkt.LEAGUE_INDEX],
+                                                                          year)
+        print(site)
+        browser.get(site)
+        page = browser.page_source
+        soup = BeautifulSoup(page, "html.parser")
 
-    for link in list_array:
-        team_link = '{}{}'.format(constants_transfermarkt.TRANSFERMARKT_URL, link)
-        browser.get(team_link)
-        soup = BeautifulSoup(browser.page_source, "html.parser")
-        team = soup.find('div', {'class': 'dataHeader dataExtended'}).find('h1', {'itemprop': 'name'}).find('span').text
+        table_teams = soup.find('div', {'id': 'yw1'})
+        teams = table_teams.find_all('a', {'class': 'vereinprofil_tooltip tooltipstered'})
+        list_array = []
+        for team in teams:
+            team_href = team.get('href')
+            if team_href not in list_array:
+                list_array.append(team_href)
 
-        players_file_name = 'data/%s- %s - Players-%s.json' % (
-            team, constants_transfermarkt.LEAGUES_LINK[constants_transfermarkt.LEAGUE_INDEX], constants_transfermarkt.YEARS[constants_transfermarkt.YEARS_INDEX])
-        players_file = open(players_file_name, 'w')
-        players_team_odd = soup.find('div', {'id': 'yw1'}).find('tbody').find_all('tr', {'class': 'odd'})
-        players_team_even = soup.find('div', {'id': 'yw1'}).find('tbody').find_all('tr', {'class': 'even'})
-        player_team = list(filter(None.__ne__, chain.from_iterable(zip_longest(players_team_odd, players_team_even))))
-        new_player_preview = transfermarkt_models.PlayerPreview()
-        for idx, players in enumerate(player_team):
-            new_player_preview.name = players.find('a', {'class': 'spielprofil_tooltip tooltipstered'}).text
-            new_player_preview.value = string_money_to_int(players.find('td', {'class': 'rechts hauptlink'}).text)
-            new_player_preview.position = players.find('table', {'class': 'inline-table'}).find_all('tr')[1].find('td').text
-            new_player_preview.birth = string_date_to_date(players.find_all('td', {'class': 'zentriert'})[1].text)
-            new_player_preview.number = players.find('div', {'class': 'rn_nummer'}).text
-            new_player_preview.url = players.find('a', {'class': 'spielprofil_tooltip tooltipstered'})['href']
-            new_player_preview.player_id = players.find('a', {'class': 'spielprofil_tooltip tooltipstered'})['id']
-            new_player_preview.team = team
-            new_player_preview.season = constants_transfermarkt.YEARS[constants_transfermarkt.YEARS_INDEX]
+        for link in list_array:
+            team_link = '{}{}'.format(constants_transfermarkt.TRANSFERMARKT_URL, link)
+            browser.get(team_link)
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+            team = soup.find('div', {'class': 'dataHeader dataExtended'}).find('h1', {'itemprop': 'name'}).find('span').text
 
-            write = '{0}'.format(
-                json.dumps(new_player_preview.__dict__, indent=4, separators=(',', ': ')))
-            if idx == 0:
-                write = "[" + write + ","
-            elif idx == (len(players) - 1):
-                write = write + "]"
-            else:
-                write = write + ","
-            players_file.write(write)
+            directory = 'data/%s/%s/' % (
+                constants_transfermarkt.LEAGUES_LINK[constants_transfermarkt.LEAGUE_INDEX], year)
+            pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+            players_file_name = directory + '%s- %s - Players-%s.json' % (
+                team, constants_transfermarkt.LEAGUES_LINK[constants_transfermarkt.LEAGUE_INDEX], year)
+            players_file = open(players_file_name, 'w')
 
-        players_file.close()
+            players_team_odd = soup.find('div', {'id': 'yw1'}).find('tbody').find_all('tr', {'class': 'odd'})
+            players_team_even = soup.find('div', {'id': 'yw1'}).find('tbody').find_all('tr', {'class': 'even'})
+            player_team = list(filter(None.__ne__, chain.from_iterable(zip_longest(players_team_odd, players_team_even))))
+            new_player_preview = transfermarkt_models.PlayerPreview()
+            for idx, players in enumerate(player_team):
+                new_player_preview.name = players.find('a', {'class': 'spielprofil_tooltip tooltipstered'}).text
+                new_player_preview.value = string_money_to_int(players.find('td', {'class': 'rechts hauptlink'}).text)
+                new_player_preview.position = players.find('table', {'class': 'inline-table'}).find_all('tr')[1].find('td').text
+                new_player_preview.birth = string_date_to_date(players.find_all('td', {'class': 'zentriert'})[1].text)
+                new_player_preview.number = players.find('div', {'class': 'rn_nummer'}).text
+                new_player_preview.url = players.find('a', {'class': 'spielprofil_tooltip tooltipstered'})['href']
+                new_player_preview.player_id = players.find('a', {'class': 'spielprofil_tooltip tooltipstered'})['id']
+                new_player_preview.team = team
+                new_player_preview.season = year
+
+                write = '{0}'.format(
+                    json.dumps(new_player_preview.__dict__, indent=4, separators=(',', ': ')))
+                if idx == 0:
+                    write = "[" + write + ","
+                elif idx == (len(players) - 1):
+                    write = write + "]"
+                else:
+                    write = write + ","
+                players_file.write(write)
+
+            players_file.close()
